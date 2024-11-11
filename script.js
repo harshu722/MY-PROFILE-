b62106163e740b2ff37c4e04f43b74d795ae94bb
Kolluru Saharsh <91141030+harshu722@users.noreply.github.com>
2024-11-11 14:11:18 +0530

    script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
+// JavaScript for password-protected download
 function checkPassword(event) {
     event.preventDefault();
     const inputPassword = document.getElementById('password').value;
     const correctPassword = "Saharsh-Harshu-2001-2024-2030";
 
     if (inputPassword === correctPassword) {
-        document.getElementById('resume-link').style.display = 'block';
+        document.getElementById('resume-link').style.display = 'block'; // Show the download link
     } else {
         alert("Incorrect password, please try again.");
     }
 }
+
 // Function to open the lightbox with the selected image
 function openLightbox(imageSrc) {
    const lightbox = document.getElementById('lightbox');
