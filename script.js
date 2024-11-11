f1e866dd94103b195c6bf7712eb3b59d0883cf29
Kolluru Saharsh <91141030+harshu722@users.noreply.github.com>
2024-11-11 13:37:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function checkPassword(event) {
+    event.preventDefault();
+    const inputPassword = document.getElementById('password').value;
+    const correctPassword = "Saharsh-Harshu-2001-2024-2030";
+
+    if (inputPassword === correctPassword) {
+        document.getElementById('resume-link').style.display = 'block';
+    } else {
+        alert("Incorrect password, please try again.");
+    }
+}
+// Function to open the lightbox with the selected image
+function openLightbox(imageSrc) {
+   const lightbox = document.getElementById('lightbox');
+   const lightboxImg = document.getElementById('lightbox-img');
+   lightboxImg.src = imageSrc; // Set the source of the image to display
+   lightbox.style.display = 'flex'; // Show the lightbox
+}
+
+// Function to close the lightbox
+function closeLightbox() {
+   const lightbox = document.getElementById('lightbox');
+   lightbox.style.display = 'none'; // Hide the lightbox
+}
